74c6d9c8a9d10c7193a5b2ff908df73e43bf76f0
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-14 16:18:46 -0400

    Continueing to update how state is passed.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 export const removeFeature = (item) => {
-  // dispatch an action here to remove an item
+  return { type: "REMOVE_FEATURE" };
 };
 
 export const buyItem = (item) => {
-  // dipsatch an action here to add an item
+  return { type: "BUY_ITEM" };
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-const Header = props => {
+const Header = (props) => {
   return (
     <>
-      <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+      <figure className='image is-128x128'>
+        <img src={props.carImageOnProps} alt={props.carNameOnProps} />
       </figure>
-      <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <h2>{props.carNameOnProps}</h2>
+      <p>Amount: ${props.carPriceOnProps}</p>
     </>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    carImageOnProps: state.car.image,
+    carNameOnProps: state.car.name,
+    carPriceOnProps: state.car.price,
+  };
+};
+
+export default connect(mapStateToProps, {})(Header);
