38c5ab3d91a0644c4947e9c2f76eb0087107a835
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-14 20:44:03 -0400

    Juan and Victoria are helping. Made some changes.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,10 @@
+export const REMOVE_FEATURE = "REMOVE_FEATURE";
+export const BUY_ITEM = "BUY_ITEM";
+
 export const removeFeature = (item) => {
-  return { type: "REMOVE_FEATURE", payload: item };
+  return { type: REMOVE_FEATURE, payload: item };
 };
 
 export const buyItem = (item) => {
-  return { type: "BUY_ITEM", payload: item };
+  return { type: BUY_ITEM, payload: item };
 };
